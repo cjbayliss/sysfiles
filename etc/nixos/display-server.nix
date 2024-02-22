47fb8f426e1785de5fe9e00c4a7148a2f24979db
Christopher Bayliss <cjbdev@icloud.com>
2024-02-23 09:16:53 +1100

    drop qtile

--- etc/nixos/display-server.nix
+++ etc/nixos/display-server.nix
@@ -6,7 +6,7 @@
 }: {
   services.xserver = {
     enable = true;
-    layout = "us";
+    xkb.layout = "us";
     exportConfiguration = true;
 
     wacom.enable = true;
@@ -15,7 +15,6 @@
       touchpad.naturalScrolling = true;
     };
 
-    windowManager.qtile.enable = true;
     windowManager.xmonad = {
       enable = true;
       enableContribAndExtras = true;
